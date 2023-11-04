9a548832055060e8b43993deff312b1dea663d60
mrtomno <adel.ngo@yahoo.com>
2023-11-04 15:22:19 -0500

    complete 2 of part 1

--- part_1/numbers.js
+++ part_1/numbers.js
@@ -1,6 +1,6 @@
-const ol = document.querySelector("ol");
+const div1 = document.querySelector("#one");
 
-const newLi = document.createElement("li");
+const p = document.createElement("p");
 
 /* newLi.innerText = "Hello!";
 
@@ -11,12 +11,49 @@ ul.append(newLi); */
 let url = "http://numbersapi.com/90?json"
 axios.get(url)
 .then(res => {
-    console.log("First promise resolved")
     console.log(res.data.text)
 
-    newLi.innerText = res.data.text;
-    ol.append(newLi)
+    p.innerText = res.data.text;
+    div1.append(p)
 })
 .catch(err => console.log("REJECTED!", err))
 
 // 2. Get data on multiple numbers in a single request
+
+const ul = document.querySelector("#two");
+const oneLi = document.createElement("li");
+const twoLi = document.createElement("li");
+const threeLi = document.createElement("li");
+const fourLi = document.createElement("li");
+
+let numberPromises = []; // to store promises
+let responseArr = []; // to store responses
+
+secondLi = document.createElement("li");
+
+for (let i = 1; i < 5; i++) {
+    numberPromises.push(
+      axios.get(`http://numbersapi.com/${i}?json`)
+    );
+  }
+
+Promise.all(numberPromises)
+    .then(numbersArr => {
+        for (res of numbersArr) {
+            console.log(res.data.text)
+            responseArr.push(res.data.text)
+        }
+
+        console.log(`length of array is ${numbersArr.length}`)
+        oneLi.innerText = responseArr[0];
+        twoLi.innerText = responseArr[1];
+        threeLi.innerText = responseArr[2];
+        fourLi.innerText = responseArr[3];
+
+        ul.append(oneLi);
+        ul.append(twoLi);
+        ul.append(threeLi);
+        ul.append(fourLi);
+  
+    })
+    .catch(err => console.log("REJECTED!", err))
